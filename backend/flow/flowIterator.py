7727fe8255346fd47cd61183c5546a07849994c9
Alessandro Bernardello <53372753+alessandroberna@users.noreply.github.com>
2025-08-21 19:13:45 +0200

    feat: threads

--- backend/flow/flowIterator.py
+++ backend/flow/flowIterator.py
@@ -1,6 +1,8 @@
 from flow.executionLog import ExecutionLog, Status
 from flow.block import Block
 import logging
+import threading
+from typing import Optional
 
 from typing import Dict, Any, List
 logger = logging.getLogger(__name__)
@@ -11,8 +13,9 @@ class FlowIterator:
         self.logs: List[ExecutionLog] = []
         self.blocks = blocks
         self.status = Status.STOPPED
+        self._thread: Optional[threading.Thread] = None
 
-    def run(self, input: Dict[str, Any]):
+    def _run_blocks(self, input: Dict[str, Any]):
         """Run all blocks and collect logs"""
         logger.debug("blocks to run: %s", [block.name for block in self.blocks])
         self.block_output = []
@@ -31,6 +34,14 @@ class FlowIterator:
                 self.logs.extend(block.get_logs())
                 logger.error(f"Block {block.name} failed: {str(e)}")
                 raise e
+    
+    def run(self, input: Dict[str, Any]):
+        if self.status == Status.RUNNING:
+            logger.warning("Runner is already running")
+            return
+        self.status = Status.RUNNING
+        self._thread = threading.Thread(target=self._run_blocks, args=(input,))
+        self._thread.start()
 
     def get_logs(self) -> List[ExecutionLog]:
         """Get all logs from the runner"""
